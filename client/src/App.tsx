0cd452484a5828a876389eef4a9a6059d43563ba
myaak <myakushek@outlook.com>
2023-04-24 10:53:11 +0300

    modified places

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import './styles/main.scss'
 import { AppRouter, Footer, Header } from './components'
 import { BrowserRouter } from 'react-router-dom'
-import { serverUrl } from './info'
-import { fetchFilms } from './store/ActionCreators/FilmsAction'
 import { useAppDispatch, useAppSelector } from './app/hooks'
 import Places from './components/Places/Places'
 
@@ -11,26 +9,6 @@ function App() {
   const openedSeanseModal = useAppSelector(state => state.seanseModal.opened)
   const dispatch = useAppDispatch()
 
-  const postFilm = async () => {
-    await fetch(`${serverUrl}/post/movie`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        title: "newMovie3",
-        durationInMinutes: "123",
-        coverURL: './',
-        description: "Cool test movie",
-        genres: []
-      })
-    }).catch((err) => {
-      console.log(err)
-      return
-    })
-  }
-
   return (
     <BrowserRouter>
       <div className="App">
@@ -38,7 +16,8 @@ function App() {
           <Header />
         </header>
         <main>
-          {openedSeanseModal && <Places />}
+          //{openedSeanseModal && <Places />}
+          <Places />
           <AppRouter />
         </main>
         <footer>
--- client/src/components/Places/Places.tsx
+++ client/src/components/Places/Places.tsx
@@ -1,42 +1,29 @@
 import './Places.scss'
 import PlacesRow from './PlacesRow'
-import {places} from './places'
-
+import { places } from './places'
+import { generatePlaces } from '../../generatePlaces'
+import { useAppSelector } from '../../app/hooks'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { serverUrl } from '../../info'
 
 
 export default function Places() {
 
-
-  const generatePlaces = (places: any) => {
-    console.log(places.length)
-    let modifiedPlaces:any = []
-    let count = 0
-    let rowPlaces: any = []
-    for (let i = 0; i < places.length + 1; ++i) {
-
-      if(count === 5) {
-
-        const placesObject = {
-          row: modifiedPlaces.length === 0 ? 1 : modifiedPlaces[modifiedPlaces.length - 1].row + 1,
-          rowPlaces: rowPlaces 
-        }
-
-        modifiedPlaces.push(placesObject)
-        count = 0
-        rowPlaces = []
-
-      }
-
-      rowPlaces.push(places[i])
-
-      count++
-    }
-
-    return modifiedPlaces
-  }
+  //const currentSession = useAppSelector(state => state.currentSession)
 
   const modifiedPlaces = generatePlaces(places)
-  console.log(modifiedPlaces)
+
+
+  const getCurrentSession = async () => {
+    //const response = await axios.get(`${serverUrl}/get/session/${currenSession.id}`)
+    //dispatch(updateCurrentSession(response.data))
+  }
+
+
+  useEffect(() => {
+    getCurrentSession()
+  }, [])
 
   return (
     <div className="places__container">
@@ -46,13 +33,20 @@ export default function Places() {
             Экран
           </div>
           <div className="places__room__web">
-            {modifiedPlaces.map((item: any, index:number) => (
-              <PlacesRow key={index} places={item.rowPlaces}/>
+            {modifiedPlaces.map((item: any, index: number) => (
+              <PlacesRow key={index} places={item.rowPlaces} />
             ))
             }
           </div>
         </div>
-        <div className="places__info"></div>
+        <div className="places__info">
+          <div className="places__info__title">
+            currentSession.title
+          </div>
+          <div className="places__info__session-info">
+            currentSession.time
+          </div>
+        </div>
       </div>
     </div>
   )
